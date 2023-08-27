e775fcc9eb51067dc49a4de0b9f70496731447d7
DARSHANR007 <darshanravichandran07@gmail.com>
2023-08-27 22:28:04 +0530

    Leetcode

---
+++ Jump Game 2/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        l = r = count = 0
+
+        while r < len(nums) - 1:
+            farthest = 0
+            for i in range(l, r + 1):
+                farthest = max(farthest, nums[i] + i)
+            l = r + 1
+            r = farthest
+            count += 1
+        return count
+
