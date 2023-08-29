6917253dfe43cd660132dc21e4cd234bd2adf254
DARSHANR007 <darshanravichandran07@gmail.com>
2023-08-29 23:25:35 +0530

    Leetcode

--- Buy and Sell Stock 2/buy and sell stock 2 solution.py
+++ Buy and Sell Stock 2/buy and sell stock 2 solution.py
@@ -1,5 +1,5 @@
 class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
+    def maxProfit(self, prices: list[int]) -> int:
         max_profit = 0
         for i in range(1, len(prices)):
             if prices[i] > prices[i-1]:
