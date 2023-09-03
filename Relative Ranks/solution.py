47e18fc62962218c0cda3ad8cded2c6c716aeab8
DARSHANR007 <darshanravichandran07@gmail.com>
2023-09-03 23:08:21 +0530

    Leetcode Sorting

---
+++ Relative Ranks/solution.py
@@ -0,0 +1,16 @@
+class Solution:
+    def findRelativeRanks(self, nums: List[int]) -> List[str]:
+        d = defaultdict(int)
+        place = ["Gold Medal", "Silver Medal", "Bronze Medal"]
+        n = len(nums)
+        a = [" "] * n
+        for i in range(n): d[nums[i]] = i
+        nums.sort(reverse=True)
+
+        for i in range(n):
+            if i < 3:
+                a[d[nums[i]]] = place[i]
+            else:
+                a[d[nums[i]]] = str(i + 1)
+
+        return a
