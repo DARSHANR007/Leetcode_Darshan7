929c5996fc18a62d7f3650c344c2fa9c0df945b2
DARSHANR007 <darshanravichandran07@gmail.com>
2023-09-17 15:32:23 +0530

    Leetcode

--- Sort Array by parity/solution.py
+++ Sort Array by parity/solution.py
@@ -16,3 +16,20 @@ class Solution:
         odd.sort()
 
         return even + odd
+#can also be done like this#
+class Solution:
+    def sortArrayByParity(self, nums: List[int]) -> List[int]:
+        left, right = 0, len(nums) - 1
+
+        while left < right:
+            if nums[left] % 2 == 1 and nums[right] % 2 == 0:
+                # Swap odd and even numbers
+                nums[left], nums[right] = nums[right], nums[left]
+            if nums[left] % 2 == 0:
+                left += 1
+            if nums[right] % 2 == 1:
+                right -= 1
+
+        return nums
+
+
