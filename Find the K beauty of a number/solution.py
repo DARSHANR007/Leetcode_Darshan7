80c224514a4328fa6f6ec6b95b15143f0b51d7c9
DARSHANR007 <darshanravichandran07@gmail.com>
2023-09-24 23:57:58 +0530

    Leetcode

---
+++ Find the K beauty of a number/solution.py
@@ -0,0 +1,8 @@
+def divisorSubstrings(self, num: int, k: int) -> int:
+    s = str(num)
+    c = 0
+    for i in range(k, len(s) + 1):
+        x = int(s[i - k:i])
+        if x != 0 and num % x == 0:
+            c += 1
+    return c
